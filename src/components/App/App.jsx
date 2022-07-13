d04da41e9a04007dec2b7b801c3f07583f55c3df
Trevor Swain <trevor.swain@cgi.com>
2022-07-13 14:52:46 +0200

    Branch made for Taranta PoC

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,19 @@
 import React, { Suspense } from 'react';
 import './App.scss';
-import ExampleComponent from '../ExampleComponent/ExampleComponent';
-/* Include if language buttons required
-import i18n from '../../services/i18n/i18n';
-*/
+import RemoteObject from '../ExampleComponent/ExampleComponent';
 
 function App() {
-  /* Code below to change the language when language buttons are clicked
-  const handleOnclick = e => {
-    e.preventDefault();
-    i18n.changeLanguage(e.target.value);
-  };
-  */
+
+  const inputs = {
+    backgroundColor: "#FF0000",
+    showAttribute: "Label",
+    font: 8
+  }
 
   return (
     <Suspense fallback="...is loading">
       <div className="App">
-        {/* Language buttons should translations need to be checked, and changing browser language is not desired
-        <div>
-          <button value="af" onClick={handleOnclick} type="submit">
-            Afrikaans
-          </button>
-          <button value="en" onClick={handleOnclick} type="submit">
-            English
-          </button>
-        </div>
-        */}
-        <ExampleComponent id="exampleComponentId" />
+        <RemoteObject.Remote id="exampleComponentId" mode="library" inputs={inputs} />
       </div>
     </Suspense>
   );
--- src/components/ExampleComponent/ExampleComponent.jsx
+++ src/components/ExampleComponent/ExampleComponent.jsx
@@ -1,19 +1,135 @@
 import React from 'react';
-import './ExampleComponent.scss';
-import { useTranslation } from 'react-i18next';
 
-const ExampleComponent = () => {
-  const { t } = useTranslation();
+export const Remote = (props) => {
+
+  const data = (props && props.mode !== "library") ? props : Definition;
+  const inputs = data.inputs;
+
+  const style = {
+    padding: "0.5em",
+    whiteSpace: "pre-wrap",
+    backgroundColor: inputs.backgroundColor,
+    color: inputs.textColor,
+    fontSize: inputs.size + "em"
+  };
+  if (inputs.font){
+    style["fontFamily"] = inputs.font;
+  }
+
+  const device = () => {
+    if (inputs.showDevice && inputs.attribute.device) 
+      return (inputs.showAttribute) ? inputs.attribute.device + "/" : inputs.attribute.device;
+    return "";
+  }
+
+  const display = () => {
+    return (inputs.attribute && inputs.attribute.label && inputs.attribute.label.length) ? inputs.attribute.label : "attributeLabel";
+  }
+
+  const values = () => {
+    return (inputs.attribute && inputs.attribute.value) ? inputs.attribute.value : "value";
+  }
 
   return (
-    <div className="exampleContainer">
-      <p>{t('language')}</p>
-      <p>{t('date_format_one', { date: new Date() })}</p>
-      <p>{t('date_format_two', { date: new Date() })}</p>
-      <p>{t('intlNumber', { val: 2000 })}</p>
-      <p>{t('dummy')}</p>
+    <div id="AttributeDisplay" style={style}>
+      <div>
+        Mode = {props.mode}
+      </div>
+      {device()}
+      {display()}
+      {(inputs.showDevice || inputs.showAttribute !== "None") && ": "}
+      {values()}
     </div>
   );
 };
 
-export default ExampleComponent;
+const Definition = {
+  idKey: "ATTRIBUTE_DISPLAY",
+  type: "ATTRIBUTE_DISPLAY",
+  name: "Remote Att. Display",
+  defaultWidth: 10,
+  defaultHeight: 2,
+  inputs: {
+    showAttribute: "Label",
+    attribute: {
+      type: "attribute",
+      label: "",
+      dataFormat: "scalar",
+      required: true
+    },
+    precision: {
+      type: "number",
+      label: "Precision",
+      default: 2
+    },
+    showDevice: {
+      type: "boolean",
+      label: "Device Name",
+      default: true           // TREVOR : false
+    },
+    showAttribute: {
+      type: "select",
+      label: "Attribute display:",
+      default: "Label",
+      options: [
+        {
+          name: "Label",
+          value: "Label"
+        },
+        {
+          name: "Name",
+          value: "Name"
+        },
+        {
+          name: "None",
+          value: "None"
+        }
+      ]
+    },
+    scientificNotation: {
+      type: "boolean",
+      label: "Scientific Notation",
+      default: false
+    },
+    showEnumLabels: {
+      type: "boolean",
+      label: "Show Enum Labels",
+      default: false
+    },
+    textColor: {
+      label: "Text Color",
+      type: "color",
+      default: "#000000"
+    },
+    backgroundColor: {
+      label: "Background Color",
+      type: "color",
+      default: "#ffffff"
+    },
+    size: {
+      label: "Text size (in units)",
+      type: "number",
+      default: 1,
+      nonNegative: true
+    },
+    font: {
+      type: "select",
+      default: "Helvetica",
+      label: "Font type",
+      options: [
+        {
+          name: "Default (Helvetica)",
+          value: "Helvetica"
+        },
+        {
+          name: "Monospaced (Courier new)",
+          value: "Courier new"
+        }
+      ]
+    }
+  }
+};
+
+const RemoteObject = { Definition: Definition, Remote: Remote };
+
+export default RemoteObject;
