82a401720ed01fe2243497259757aef2705b0efd
gallacherc <chloe.gallacher@cgi.com>
2023-08-24 17:18:13 +0100

    NAL-687: Refactoring cypress config use of mount command

--- src/components/ReactSkeleton/ReactSkeleton.cy.tsx
+++ src/components/ReactSkeleton/ReactSkeleton.cy.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { mount } from 'cypress/react18';
 import ReactSkeleton from './ReactSkeleton';
 
 describe('<ReactSkeleton />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     // eslint-disable-next-line no-undef
-    mount(<ReactSkeleton />);
+    cy.mount(<ReactSkeleton />);
   });
 });
