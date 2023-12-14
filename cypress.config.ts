c150810656a0567e77ecf43ad99b2bc43844d6f2
Ugur Yilmaz <ugur.yilmaz@skao.int>
2023-12-14 12:11:51 +0000

    STAR-145: add spec pattern

--- cypress.config.ts
+++ cypress.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
 
   e2e: {
     "supportFile": "tests/cypress/support/e2e.js",
+    "specPattern": "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config);
       on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
