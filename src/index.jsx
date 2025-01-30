36d060f5bf6f2e7fd78acb8ca0278851976fcc69
Andre Odendaal <andre@vivosa.co.za>
2025-01-30 14:59:01 +0200

    STAR-1022: Added scr script to index.html

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './services/i18n/i18n';
 import { StoreProvider } from '@ska-telescope/ska-gui-local-storage';
 import { AuthProvider } from '@ska-telescope/ska-login-page';
-// import { MSENTRA_CLIENT_ID, MSENTRA_TENANT_ID, MSENTRA_REDIRECT_URI } from './utils/constants';
-
-// TODO : USE THE ABOVE 
+import { MSENTRA_CLIENT_ID, MSENTRA_TENANT_ID, MSENTRA_REDIRECT_URI } from './utils/constants';
 
 import App from './components/App/App';
 import Loader from './components/Loader/Loader';
@@ -17,9 +15,9 @@ root.render(
   <React.Suspense fallback={<Loader />}>
     <StoreProvider>
     <AuthProvider
-        MSENTRA_CLIENT_ID="bdbd5ae8-b9ec-494a-a74d-39245a0af01b"
-        MSENTRA_AUTHORITY="https://login.microsoftonline.com/78887040-bad7-494b-8760-88dcacfb3805"
-        MSENTRA_REDIRECT_URI="http://localhost:8090/"
+        MSENTRA_CLIENT_ID={MSENTRA_CLIENT_ID}
+        MSENTRA_AUTHORITY="https://login.microsoftonline.com/"{...MSENTRA_TENANT_ID}
+        MSENTRA_REDIRECT_URI={MSENTRA_REDIRECT_URI}
       >
 
         <App />
